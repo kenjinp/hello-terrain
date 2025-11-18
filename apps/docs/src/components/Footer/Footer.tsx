ad7ca18ae1375a921adb093761a332c66499626e
Kenny Pirman <kenny.pirman@character.ai>
2025-11-18 18:50:24 -0500

    no need for this I guess

--- apps/docs/src/components/Footer/Footer.tsx
+++ apps/docs/src/components/Footer/Footer.tsx
@@ -5,7 +5,6 @@ export const Footer = () => {
   return (
     <footer className="text-sm opacity-50 py-4 text-shadow-md text-center flex flex-row items-center justify-center gap-2">
       <div>
-        Copyright © 2025{" "}
         <Link href="https://kenny.wtf" className="underline">
           Kenneth Pirman
         </Link>
